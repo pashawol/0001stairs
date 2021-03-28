a20f97578e57a44938ca2e5d99d52dbd29efe8b7
MaxSimakov <Simakov.maxim82@gmail.com>
2021-03-28 21:10:03 +0300

    fixed some problems

--- public/js/common.js
+++ public/js/common.js
@@ -324,8 +324,8 @@ $(document).ready(function () {
 		dots: false,
 		infinite: true,
 		speed: 300,
-		slidesToShow: 1,
-		centerMode: true,
+		// slidesToShow: 1,
+		// centerMode: true,
 		variableWidth: true,
 		arrows: false
 	});
